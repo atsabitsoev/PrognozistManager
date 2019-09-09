e517e88af15dcaf94a24744ad24764d6aa152838
Ацамаз Бицоев <atsamazbitsoev@yandex.ru>
2019-09-09 21:13:55 +0300

    deeded

--- PrognozistManager/ViewController.swift
+++ PrognozistManager/ViewController.swift
@@ -7,14 +7,65 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var tfMainTitle: UITextField!
+    @IBOutlet weak var tfAdditionalTitle: UITextField!
+    @IBOutlet weak var tfForecastName: UITextField!
+    @IBOutlet weak var tfMultiplier: UITextField!
+    @IBOutlet weak var tfPartOfBank: UITextField!
+    @IBOutlet weak var tfEventDate: UITextField!
+    @IBOutlet weak var tvAuthorsDescribtion: UITextView!
+    @IBOutlet weak var datePickerDay: UIDatePicker!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
+    
+    
+    private func sendDataToFireStore() {
+        
+        let db = Firestore.firestore()
+        
+        let forecastDateString = getDateInString(from: datePickerDay.date)
+        db.collection(forecastDateString).addDocument(data: ["mainTitle" : tfMainTitle.text ?? "",
+            "additionalTitle" : tfAdditionalTitle.text ?? "",
+            "forecastName" : tfForecastName.text ?? "",
+            "multiplier" : tfMultiplier.text ?? "",
+            "partOfBank" : tfPartOfBank.text ?? "",
+            "date" : tfEventDate.text ?? "",
+            "authorsDescribtion" : tvAuthorsDescribtion.text ?? "",
+            "authorsName" : ["Алексей Орлов",
+                             "Артем Остапов",
+                             "Айваз Магомедов"].randomElement()!])
+    }
+    
+    
+    private func getDateInString(from date: Date) -> String {
+        
+        let calendar = Calendar(identifier: .gregorian)
+        let timeZoneMoscow = TimeZone(identifier: "Europe/Moscow")!
+        let dateComponentsToday = calendar.dateComponents(in: timeZoneMoscow, from: date)
+        guard let day = dateComponentsToday.day,
+            let month = dateComponentsToday.month,
+            let year = dateComponentsToday.year else {
+                return ""
+        }
+        
+        return "\(day)\\\(month)\\\(year)"
+    }
+    
+    
+    @IBAction func butSendTapped(_ sender: UIButton) {
+        
+        sendDataToFireStore()
+    }
+    
 
 }
 
