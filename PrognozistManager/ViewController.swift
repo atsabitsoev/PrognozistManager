301180fd97a2821d5765fa6057718a10eee66123
Ацамаз Бицоев <atsamazbitsoev@yandex.ru>
2019-09-09 18:32:11 +0300

    Initial Commit

---
+++ PrognozistManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PrognozistManager
+//
+//  Created by Ацамаз Бицоев on 09/09/2019.
+//  Copyright © 2019 Ацамаз Бицоев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
